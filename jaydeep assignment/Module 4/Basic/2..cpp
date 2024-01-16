182648f3491fe77f716cb89b5256093f2553da97
jaydeeptank28 <jaydeeptank61@gmail.com>
2024-01-16 16:36:03 +0530

    done

---
+++ jaydeep assignment/Module 4/Basic/2..cpp
@@ -0,0 +1,11 @@
+/*OOP stands for Object-Oriented Programming, which is a programming paradigm
+ that uses objects, classes, and various principles to design and organize code. 
+ 
+ The main concepts of OOP include:
+ 
+ 1.Class
+ 2.Object
+ 3.Encapsulation
+ 4.Inheritance
+ 5.Polymorphism:
+ 6.Abstraction
---
+++ jaydeep assignment/Module 4/Basic/3.cpp
@@ -0,0 +1,3 @@
+/*OOP refers to Object Oriented Programming. It deals with objects 
+  and their properties. POP refers to Procedural Oriented Programming
+  and deals with programs and functions
